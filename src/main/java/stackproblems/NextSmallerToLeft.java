def2332b5fa74b0e61db07a9c15712c84e90a1ea
kanchisamudramkiran <kanchisamudramkiran@gmail.com>
2021-09-24 12:31:21 +0530

    NextSmallerToLeft

--- src/main/java/stackproblems/NextSmallerToLeft.java
+++ src/main/java/stackproblems/NextSmallerToLeft.java
@@ -1,4 +1,45 @@
 package stackproblems;
 
 public class NextSmallerToLeft {
+    public  static void main(String[] args){
+//        int arr[] = {11,13,21,3};
+        int arr[] = {4,5,2,10,8};
+        StackMethod(arr);
+    }
+
+    static  void simpleMethod(int arr[]){
+        int i, j, n, next;
+        n = arr.length;
+    for(i =0; i< n;i++){
+        next =-1;
+        for(j=i;j>=0; j--){
+            if(arr[j] < arr[i]){
+                next = arr[j];
+                break;
+            }
+        }
+        System.out.println(arr[i]+"------------->"+next);
+    }
+    }
+
+    static  void StackMethod(int[] arr){
+        int n = arr.length;
+        Stack s = new Stack(n);
+        int i, j;
+        int[] output = new int[n];
+        for(i =0;i<n;i++){
+
+            if(!s.isEmpty()){
+                while (!s.isEmpty() && s.peek() > arr[i]){
+                    s.pop();
+                }
+            }
+            output[i] = s.isEmpty() ? -1: s.peek();
+            s.push(arr[i]);
+        }
+
+        for(i= 0; i<n;i++){
+            System.out.println(arr[i]+"-------->"+output[i]);
+        }
+    }
 }
